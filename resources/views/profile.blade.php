e7a3ceb70c8f26cddc40e404b9e9f5beb76b9948
haisenheim <clementessomba@gmail.com>
2024-10-02 14:31:33 +0100

    first

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -19,20 +19,27 @@
 @endsection
 
 @section('content')
-    <div class="card">
+    <div style="margin: 1px auto; width:500px" class="card">
         <div class="card-body">
+            <div>
+                <img src="{{ $user->photo }}" alt="">
+            </div>
             <form enctype="multipart/form-data" action="{{ route('profile.store') }}" method="post">
                 @csrf
-                <div class="d-flex gap-2 flex-grow">
-                        <div class=" w-75">
+                <div class="">
+                        <div class="">
                             <label for="">NOM DE L'UTILISATEUR</label>
-                            <input required type="text" name="username" placeholder="Saisir le nom de l'utilisateur" class="form-control">
+                            <input required type="text" name="name" value="{{ $user->name }}" placeholder="Saisir le nom de l'utilisateur" class="form-control">
+                        </div>
+                        <div class="">
+                            <label for="">PHOTO DE PROFIL</label>
+                            <input required type="file" name="photo"  placeholder="Saisir le nom de l'utilisateur" class="form-control">
                         </div>
                     </div>
-                    <div class="d-flex gap-2 flex-grow">
-                        <div class="form-group w-50">
+                    <div class="">
+                        <div class="">
                             <label for="">EMAIL DE CONNEXION</label>
-                            <input required type="email" name="email" placeholder="Saisir l'adresse email de connexion de l'utilisateur" class="form-control">
+                            <input required type="email" name="email" value="{{ $user->email }}" placeholder="Saisir l'adresse email de connexion de l'utilisateur" class="form-control">
                         </div>
                         <div class="form-group w-50">
                             <label for="">MOT DE PASSE</label>
